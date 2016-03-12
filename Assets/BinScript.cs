8e49d4c430793bf7a0fa4715d24790c05f12ba27
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-03-12 19:59:21 +0000

    HCI Prototype
    Main Features added

---
+++ Assets/BinScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BinScript : MonoBehaviour {
+
+	//when collider stays in the bin trigger
+	void OnTriggerStay2D(Collider2D coll)
+	{
+		//if the collider gameobject name is item
+		if (coll.gameObject.tag == "Item") 
+		{
+			//finds the items type and checks if it is in the correct bin
+			string thisItemType = coll.gameObject.GetComponent<FoodScript>().itemType;
+			if (thisItemType == "Glass" || thisItemType == "Plastic" || thisItemType == "Paper") 
+			{
+				if (this.gameObject.name == "Blue")
+				{
+				//destroys the object and adds score
+				Destroy(coll.gameObject);
+				//add score
+				}
+				else
+				{
+					//if fails then the end screen is played 
+					Application.LoadLevel("End Scene");
+					
+				}
+			}
+			else if (thisItemType == "Food" )
+			{
+				if(this.gameObject.name == "Green")
+				{
+				Destroy(coll.gameObject);
+				//add score
+				}
+				else
+				{
+					//if fails then the end screen is played 
+					Application.LoadLevel("End Scene");
+					
+				}
+			}
+			else if (thisItemType == "Misc" )
+			{
+				if(this.gameObject.name == "Trash")
+				{
+				Destroy(coll.gameObject);
+				//add score
+				}
+				else
+				{
+					//if fails then the end screen is played 
+					Application.LoadLevel("End Scene");
+					
+				}
+			}
+
+
+
+		}
+	}
+}
---
+++ Assets/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	private bool pause = false;
+
+	public void GoToMenu()
+	{
+		Application.LoadLevel("Main Menu");
+	}
+	public void GoToHelp()
+	{
+		Application.LoadLevel("Help Menu");
+	}
+
+	public void GoToLevel()
+	{
+		Application.LoadLevel("Game");
+
+	}
+	public void GoToEnd()
+	{
+		Application.LoadLevel("End Scene");
+
+	}
+
+
+
+	
+}
